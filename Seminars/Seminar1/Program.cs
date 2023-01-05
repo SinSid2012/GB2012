3f6e85e5a8746e8ed7b40fb328d111ffc54e2bd7
SinSid2012 <sinsid2012@gmail.com>
2023-01-05 20:40:27 +0500

    Seminar1Start

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Console.WriteLine(" Input Number ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = x * x;
+// Console.WriteLine($"Quad of {x} > {y}");
+// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// a = 25; b = 5 -> да
+// a = 2 b = 10 -> нет
+// a = 9; b = -3 -> да
+// a = -3 b = 9 -> нет
+
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int quad = b * b;
+
+// if(quad == a)
+// {
+//     Console.WriteLine($"{a} is quad of {b}");
+// }
+// else
+// {
+//    Console.WriteLine($"{a} is not quad of {b}");  
+// }
