2cf83f4d4c9e893dbce9c60a1b9a799117dfd21c
SinSid2012 <sinsid2012@gmail.com>
2023-01-08 21:49:18 +0500

    Homeworks4

---
+++ Homeworks/Homeworks4/Program.cs
@@ -0,0 +1,100 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// int numberA = ReadInt("Введите число A: ");
+// int numberB = ReadInt("Введите число B: ");
+// DegreeNum(numberA, numberB);
+
+// void DegreeNum(int a, int b)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= b; i++)
+//     {
+//         result = result * a;
+//     }
+//     Console.WriteLine(result);
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.WriteLine(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// int number = ReadInt("Введите число A: ");
+// int len = NumberLen(number);
+// SumNumbers(number, len);
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int NumberLen(int a)
+// {
+//     int index = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         index++;
+//     }
+//     return index;
+// }
+
+// void SumNumbers(int n, int len)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= len; i++)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     Console.WriteLine(sum);
+// }
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] myArray = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         myArray[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return myArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     Console.WriteLine("Полученный масив -> ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Введите размер массива: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// ShowArray(CreateRandomArray(length, min, max));
---
+++ Seminars/Seminar4/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+// int FindSum (int num)
+// {
+//     int sum = 0;
+//     for (int current = 1; current <= num; current++)
+//     {
+//         sum += current;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Input A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int result = FindSum(a);
+// Console.WriteLine(result);
+
+// задача 2. Напишите программу, которая выводит массив из N элементов, заполненный заданными числами в случайном порядке.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] myArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return myArray;
+}
+
+void ShowArray(int[] array)
+{
+    Console.WriteLine("Полученный масив -> ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально возможное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально возможное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateRandomArray(length, min, max);
+// ShowArray(newArray);
+
+ShowArray(CreateRandomArray(length, min, max));
