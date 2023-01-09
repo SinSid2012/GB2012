17e32777ed478574afe86fb9e88406b43a442d6a
SinSid2012 <sinsid2012@gmail.com>
2023-01-09 22:53:15 +0500

    Seminar5

---
+++ Seminars/Seminar5/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задание 1. Найти сумму положительных и отрицательных элементов.
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] myArray = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         myArray[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return myArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     Console.WriteLine("Полученный масив -> ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void FindMinMaxSum (int[] array)
+// {
+//     int sumPositive = 0;
+//     int sumNegative = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0) sumPositive += array[i];
+//         else
+//         sumNegative += array[i];
+//     }
+//     Console.WriteLine($"Сумма положительных элементов {sumPositive}, а отрицательных {sumNegative}");
+// }
+
+// Console.WriteLine("Введите размер массива: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateRandomArray(length, min, max);
+
+// ShowArray(newArray);
+// FindMinMaxSum(newArray);
+
+// Задача 2. Напишите программу замены элементов массива. Положительные элементы замените на соответсвующие отрицательные и наоборот.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] myArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return myArray;
+}
+
+void ShowArray(int[] array)
+{
+    Console.WriteLine("Полученный масив -> ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] ChangeElemets (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+    return array;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально возможное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально возможное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(length, min, max);
+
+ShowArray(newArray);
+newArray = ChangeElemets(newArray);
+ShowArray(newArray);
